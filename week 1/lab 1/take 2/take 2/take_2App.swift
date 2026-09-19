9bc0e11837676ad651420564d4c4af27d8c91871
1761-cpu <vc.anh1761@gmail.com>
2026-09-19 14:49:35 +0700

    Add files via upload

---
+++ week 1/lab 1/take 2/take 2/take_2App.swift
@@ -0,0 +1,17 @@
+//
+//  take_2App.swift
+//  take 2
+//
+//  Created by SEIU iMac 5 on 19/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct take_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
