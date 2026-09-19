5d94b6909a478daf4a1e6c08db286fc3fbafcf1b
1761-cpu <vc.anh1761@gmail.com>
2026-09-19 16:26:47 +0700

    Add files via upload

---
+++ week 1/HW1 - Ex2/Ex2/Ex2/Ex2App.swift
@@ -0,0 +1,17 @@
+//
+//  Ex2App.swift
+//  Ex2
+//
+//  Created by SEIU iMac 5 on 19/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct Ex2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
